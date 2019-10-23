d5d68d3591f0d46bbe2c504dd97d968b296559d2
Erik van Bilsen <erik@grijjy.com>
2019-10-23 12:57:06 -0700

    Fixed dependency on Sample.Androidapi.AppGlue for non-Android platforms

--- Tutorials/1.GettingStarted/1.HelloWindow/T101HelloWindow.dpr
+++ Tutorials/1.GettingStarted/1.HelloWindow/T101HelloWindow.dpr
@@ -6,8 +6,7 @@ uses
   Sample.App in '..\..\Common\Sample.App.pas',
   Sample.Platform in '..\..\Common\Sample.Platform.pas',
   App in 'App.pas',
-  Sample.Classes in '..\..\Common\Sample.Classes.pas',
-  Sample.Androidapi.AppGlue in '..\..\Common\Sample.Androidapi.AppGlue.pas';
+  Sample.Classes in '..\..\Common\Sample.Classes.pas';
 
 begin
   RunApp(THelloWindowApp, 800, 600, 'Hello Window');
